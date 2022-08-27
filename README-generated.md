3578b089d49814051df489df670b0ee2d26dc583
yxx-dev <hermitlun@gmail.com>
2022-08-26 17:51:53 -0700

    initial README

--- README-generated.md
+++ README-generated.md
@@ -1,4 +1,31 @@
-# rrr
-## Description
+# 
 
-ttt
+## Usage
+f
+
+## How to contribute
+fg
+
+## License
+MIT
+
+## Tests
+g
+
+## GitHub
+
+
+## Email
+g
+
+## Questions
+g
+
+## Description
+gf
+
+## Table of Contents
+pending
+
+## Installation
+g
--- index.js
+++ index.js
@@ -5,36 +5,12 @@ import fs from 'fs';
 let readMeFileName = 'README-generated';
 //an array of questions for user input
 const questions = ['What is the title of your project?', 'Enter project descriptions:', 'Enter installation instructions:', 'Enter usage info:', 'Enter contribution guidelines:', 'Enter test instructions:', 'License:', 'Enter GitHub username:', 'Enter email address:', 'Questions'];
-
-
+//const subtitles = ['## Table of Contents', '## Description', '## Installation', '## Usage', '## How to Contribute', '## Tests', '## License', '## GitHub', '## Email', '## Questions'];
+let userAnswers;
 // 'Enter Table of Contents:'?
 
 
-
-
-
-//function to write README file
-function writeToFile(fileName, title) {
-    //create or clear readMeFileName file
-    fs.writeFile(`./${fileName}.md`, '', err => err ? console.error(err) : console.log(`${fileName}.md file created`));
-    //writte title
-    fs.writeFile(`./${fileName}.md`, title, err => err ? console.error(err) : console.log('title recorded'));
-}
-
-//function to append to README file
-function appendToFile(fileName, data) {
-     //append info to file
-    fs.appendFile(`./${fileName}.md`, data, err => err ? console.error(err) : console.log('data recorded'));
-}
-
-//function to initialize app
-function init() {
-    //writeToFile(process.argv[2], process.argv[3]);
-
-    //for (i=0; i<questions.length; i++) {
-
-   //};
-    inquirer
+inquirer
     .prompt([
         {
             type: 'input',
@@ -89,12 +65,74 @@ function init() {
         },
     ])
     .then(answers => {
-        //console.log(answers);
-        writeToFile(readMeFileName,`# ${answers.title}\n`);
-        appendToFile(readMeFileName, `\n## Description\n${answers.description}\n`);
-        });
+        userAnswers=answers;
+        
+    }).then(() => {init();});
     //.catch(err ? err => console.log(err) : console.log('recorded'));
+
+
+//function to write README file
+function writeToFile(fileName, title) {
+    //create or clear readMeFileName file
+    fs.writeFile(`./${fileName}.md`, '', err => err ? console.error(err) : console.log(`${fileName}.md file created`));
+    //writte title
+    fs.writeFile(`./${fileName}.md`, `# ${title}\n`, err => err ? console.error(err) : console.log('title recorded'));
 }
 
+//function to append to README file
+function appendToFile(fileName, subtitle, data) {
+     //append info to file
+    fs.appendFile(`./${fileName}.md`, `\n${subtitle}\n${data}\n`, err => err ? console.error(err) : console.log('data recorded'));
+}
+
+
+
+
+
+//function to initialize app
+function init() {
+    writeToFile(readMeFileName, userAnswers.title);
+    appendToFile(readMeFileName, '## Description', userAnswers.description);
+
+    appendToFile(readMeFileName, '## Table of Contents','pending');
+    
+    appendToFile(readMeFileName, '## Installation', userAnswers.installation);
+    appendToFile(readMeFileName, '## Usage', userAnswers.usage);
+    appendToFile(readMeFileName, '## How to contribute', userAnswers.contribution);
+    appendToFile(readMeFileName, '## Tests', userAnswers.test);
+    appendToFile(readMeFileName, '## License', userAnswers.license);
+    appendToFile(readMeFileName, '## GitHub', userAnswers.github);
+    appendToFile(readMeFileName, '## Email', userAnswers.email);
+    appendToFile(readMeFileName, '## Questions', userAnswers.questions);
+
+    //writeToFile(process.argv[2], process.argv[3]);
+    /*
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        }
+    ])
+    .then(answers => writeToFile(readMeFileName,`# ${answers.title}\n`));
+
+    for (let i=1; i<questions.length; i++) {
+        inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: questions[i],
+                name: i,
+            }
+        ])
+        .then(answers => appendToFile(readMeFileName, subtitles[i], answers.i))
+       };
+
+    */
+}
+
+
 // Function call to initialize app
-init();
+//init();
+
